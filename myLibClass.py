7ed7d7e6e502f73d978320264122508e31d2acd2
phs-lab <59041349+phs-lab@users.noreply.github.com>
2020-11-09 01:46:39 +0900

    Update myLibClass.py

--- myLibClass.py
+++ myLibClass.py
@@ -1288,5 +1288,5 @@ if importlib.util.find_spec("rpy2"):
     import rpy2.robjects as ro  # 이름 충돌 방지를 위해 ro 사용. 즉, 『 rstr = "R 명령어"; ro.r(rstr)』와 같은 방식으로 사용
 
     print('【rpy2】', rpy2.__version__, ": 최초⇒'%load_ext rpy2.ipython', 다시 load(내부 R 세션 시작)⇒'%reload_ext rpy2.ipython'; %Rㆍ%%R == ro.r('R Script')")
-    print(" %Rget,%R -i,%Rpush ⇔ %R -o,%Rpull ☞『df』 ①py⇒r: ro.r.assign('R.df',ro.pandas2ri.py2ri(PYdf)), ②r⇒py: PYdf=ro.pandas2ri.ri2py(R.df)")
+    print(" %Rget,%R -i,%Rpush ⇔ %R -o,%Rpull ☞『df』 ①py⇒r:ro.r.assign('R.df',ro.pandas2ri.py2ri(PYdf)), ②r⇒py:PYdf=ro.pandas2ri.ri2py(R.df)")
     
