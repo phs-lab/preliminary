9ceaa4a2899793d5410cfad8caecfa8f1144bd44
phs-lab <59041349+phs-lab@users.noreply.github.com>
2021-02-11 11:32:55 +0900

    Update myLibClass.py

--- myLibClass.py
+++ myLibClass.py
@@ -297,9 +297,9 @@ class my:  # import myLibClass; my = myLibClass.myLib()
   def pdDescribe(cls, df):  # pandas dataframe 합치기 : https://antilibrary.org/2483
     if type(pd.DataFrame()) == type(df):
       DESC = df.describe()  # 정규성 검정 https://data-newbie.tistory.com/442, https://bioinformaticsandme.tistory.com/37
-      CV = pd.DataFrame({'CV':fish.mean() / fish.std()})
-      MODE = df[df.describe().columns].mode().head(1).T; MODE.columns = ['Mode']
-      SnK = pd.concat({'Skewness':df.skew(), 'Kurtosis':df.kurtosis()}, axis=1)
+      CV = pd.DataFrame({'cv':fish.mean() / fish.std()})
+      MODE = df[df.describe().columns].mode().head(1).T; MODE.columns = ['mode']
+      SnK = pd.concat({'skewness':df.skew(), 'kurtosis':df.kurtosis()}, axis=1)
       rtn = pd.concat([pd.concat([pd.concat([DESC.T, MODE], axis=1), CV], axis=1), SnK], axis=1)
     else:
       rtn = pd.DataFrame({'Error': "Argument {0} != pd.DataFrame".format(type(df))}, index=[0])
